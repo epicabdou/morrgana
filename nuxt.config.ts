10cc9db20f523730794582ffe44745a78321ed2f
abdl'hamid <113943477+epicabdou@users.noreply.github.com>
2025-07-04 20:29:58 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,22 +12,22 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      pocketbaseUrl: process.env.POCKETBASE_URL || 'http://127.0.0.1:8090',
-      siteUrl: process.env.SITE_URL || 'http://localhost:3000',
+      pocketbaseUrl: process.env.POCKETBASE_URL || '',
+      siteUrl: process.env.SITE_URL || '',
       googleClientId: process.env.GOOGLE_CLIENT_ID || '',
-      tinymceApiKey: process.env.TINYMCE_API_KEY || 'cej84b497evzdt54m9tgdpm8cnozk1hbzlm63q2teujzd41o'
+      tinymceApiKey: process.env.TINYMCE_API_KEY || ''
     }
   },
     app: {
     head: {
-      title: 'Admin Dashboard',
+      title: 'Morrgana',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'E-commerce Admin Dashboard' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap' }
